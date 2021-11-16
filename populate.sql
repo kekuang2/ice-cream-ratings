7c2bef087674292d803673ba3d7d00879a56aac8
Kevin Kuang <kevinkuang0@gmail.com>
2021-11-15 22:51:26 -0800

    created tables

---
+++ populate.sql
@@ -0,0 +1,14 @@
+.mode "csv"
+.separator ","
+.header off
+
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/talenti/products.csv talenti
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/talenti/reviews.csv talentiReviews
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/breyers/products.csv breyers
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/breyers/reviews.csv breyersReviews
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/hd/products.csv hd
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/hd/reviews.csv hdReviews
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/bj/products.csv bj
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/bj/reviews.csv bjReviews
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/combined/products.csv combinedProducts
+.import /Users/kevinkuang/Desktop/CSE111/Project/data/combined/reviews.csv combinedReviews
